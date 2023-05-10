10af8d5319ca062185980be74c11139f06716c6e
zahratalaei <zahratalaei@users.noreply.github.com>
2023-05-10 14:38:14 +1000

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/scenes/navbar/Navbar"
+import { useState } from "react"
+import { SelectedPage } from "./shared/types"
+
+function App() {
+  const[selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  return (
+    <div className='app bg-gray-20'>
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/shared/types.ts
@@ -0,0 +1,6 @@
+export enum SelectedPage {
+     Home = "home",
+     Benefits = "benefit",
+     OurClasses = "ourClasses",
+     ContactUs = "contactUs",
+}
