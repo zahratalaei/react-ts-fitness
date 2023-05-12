30464e363c97cfa43784b8f1d23aa01982e5b6df
zahratalaei <zahratalaei@users.noreply.github.com>
2023-05-12 20:33:45 +1000

    added all sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import Navbar from "@/scenes/navbar/Navbar"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { SelectedPage } from "./shared/types"
+import Home from "./scenes/home/Home"
+import Benefits from "./scenes/benefits/Benefits"
+import OurClasses from "./scenes/ourClasses/OurClasses"
+import ContactUs from "./scenes/contactUs/ContactUs"
+import Footer from "./scenes/footer/Footer"
 
 function App() {
+  const[isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
   const[selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+
+useEffect(()=>{
+  const handleScroll = () => {
+    if(window.scrollY === 0) {
+      setIsTopOfPage(true)
+      setSelectedPage(SelectedPage.Home)
+    }
+    if(window.scrollY !== 0) setIsTopOfPage(false)
+  }
+  window.addEventListener("scroll", handleScroll)
+  return () => window.removeEventListener("scroll", handleScroll)
+},[])
+
   return (
     <div className='app bg-gray-20'>
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} isTopOfPage={isTopOfPage}/>
+      <Home setSelectedPage={setSelectedPage}/>
+      <Benefits setSelectedPage={setSelectedPage}/>
+      <OurClasses setSelectedPage={setSelectedPage}/>
+      <ContactUs setSelectedPage={setSelectedPage}/>
+      <Footer/>
     </div>
   )
 }
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -4,3 +4,14 @@ export enum SelectedPage {
      OurClasses = "ourClasses",
      ContactUs = "contactUs",
 }
+export interface BenefitType {
+     icon: JSX.Element;
+     title: string;
+     description: string;
+}
+
+export interface ClassType {
+     name: string;
+     description?: string;
+     image:string;
+}
