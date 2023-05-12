cec172eff9821fe4f25ec5775634b601e1bafb5a
zahratalaei <zahratalaei@users.noreply.github.com>
2023-05-12 22:03:00 +1000

    fix url

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/finalRepo",
+  // base: "/finalRepo",
   plugins: [react()],
   resolve: {
     alias:[{find: "@", replacement: path.resolve(__dirname,"src")}]
   },
+  base: '/vite-deploy-demo/'
   
 })
